6e5c3e78f6026cf719fd7b429a07a229fc217743
Nishiyama13 <nishiyamaeel@gmail.com>
2023-01-07 19:22:38 -0300

    post tweets

--- src/app.js
+++ src/app.js
@@ -1,6 +1,5 @@
 import express from "express";
 import cors from "cors";
-//const msg = "Teste";
 
 const server = express();
 server.use(express.json());
@@ -28,5 +27,20 @@ server.post("/sign-up", (req, res) => {
   res.send("OK");
 });
 
-const PORT = 5023;
+server.post("/tweets", (req, res) => {
+  const { username, tweet } = req.body;
+
+  const userExists = users.find(user => user.username === username);
+
+  if (!userExists) {
+    return res.status(401).send("UNAUTHORIZED");
+  }
+
+  const newTweet = { username, tweet };
+  tweets.push(newTweet);
+  console.log(tweets);
+  return res.send("OK");
+});
+
+const PORT = 5000;
 server.listen(PORT, () => console.log(`Servidor rodando na porta: ${PORT}`));
