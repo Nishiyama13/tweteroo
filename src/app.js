244eea847871a4ea95f052dcdd9cb6d505ac6019
Nishiyama13 <nishiyamaeel@gmail.com>
2023-01-07 18:29:04 -0300

    add new user

--- src/app.js
+++ src/app.js
@@ -1,9 +1,24 @@
 import express from "express";
-//import cors from "cors"
+import cors from "cors";
+//const msg = "Teste";
 
 const server = express();
-server.get("/teste", (req, res) => {
-  res.send("Teste");
+server.use(express.json());
+server.use(cors());
+
+const users = [
+  {
+    username: "bobesponja",
+    avatar:
+      "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info",
+  },
+];
+
+server.post("/sign-up", (req, res) => {
+  const newUser = req.body;
+  users.push(newUser);
+  res.send(users);
 });
-const PORT = 5000;
-server.listen(PORT, () => console.log(`Servidor rodando na porta: ${5000}`));
+
+const PORT = 5023;
+server.listen(PORT, () => console.log(`Servidor rodando na porta: ${PORT}`));
